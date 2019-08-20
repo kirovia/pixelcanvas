1e41e244769ba21832f4b3423d0ae0fdd9e750e6
Joseph <josephandrewphillips@gmail.com>
2019-08-19 22:57:14 -0700

    Add color select functionality

--- main.js
+++ main.js
@@ -4,6 +4,38 @@ var pixel = document.querySelectorAll('.box');
 // Moving mouse draws on canvas when true
 var drawing = false;
 
+// Variable for color select
+var color = 'default'
+
+// Color selection
+document.getElementById('black').addEventListener('click', function() {
+  color = 'black';
+});
+document.getElementById('red').addEventListener('click', function() {
+  color = 'red';
+});
+document.getElementById('orange').addEventListener('click', function() {
+  color = 'orange';
+});
+document.getElementById('yellow').addEventListener('click', function() {
+  color = 'yellow';
+});
+document.getElementById('green').addEventListener('click', function() {
+  color = 'green';
+});
+document.getElementById('cyan').addEventListener('click', function() {
+  color = 'cyan';
+});
+document.getElementById('blue').addEventListener('click', function() {
+  color = 'blue';
+});
+document.getElementById('magenta').addEventListener('click', function() {
+  color = 'magenta';
+});
+document.getElementById('white').addEventListener('click', function() {
+  color = 'white';
+});
+
 // Sensing mouse movements
 pixel.forEach(function(pixel) {
   pixel.addEventListener('mousedown', function() {
@@ -14,12 +46,8 @@ pixel.forEach(function(pixel) {
 pixel.forEach(function(pixel) {
   pixel.addEventListener('mousemove', function() {
     if (drawing === true) {
-      var r = Math.floor(Math.random() * 255);
-      var g = Math.floor(Math.random() * 255);
-      var b = Math.floor(Math.random() * 255);
-      var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
-      pixel.style.backgroundColor = rgb
-    };
+      pixel.style.backgroundColor = color;
+    }
   });
 });
 
